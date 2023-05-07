1ee3238fa7bfa15c323935ee8a2b2f404e0e9516
xiongwei <weiweixiong914@163.com>
2023-05-07 18:11:16 +0800

    env

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // reactStrictMode: true,
-  basePath: '/weini/base',
-  assetPrefix: '/weini/base/',
+  // basePath: '/weini/base',
+  // assetPrefix: '/weini/base/',
 };
 
 module.exports = nextConfig;
